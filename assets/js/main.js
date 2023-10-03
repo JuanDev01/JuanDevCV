4f229972dfc6c35ce0d32d1cd2a0969d9ffb2d0d
JuanDev01 <113712035+JuanDev01@users.noreply.github.com>
2023-10-03 14:02:41 -0700

    updated APIs Section including NASAs and Meme Generator APIs

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,11 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+// const express = require('express');
+// const app = express()
+// const cors = require('cors')
+// app.use(cors())
+
 (function($) {
 
 	var	$window = $(window),
@@ -119,6 +124,81 @@
 					target: $body,
 					visibleClass: 'header-visible'
 				});
+
+
+// API's Code Starts Here 
+
+document.querySelector('.button-API').addEventListener('click', getFetch)
+
+async function getFetch(){
+	// const choice = document.querySelector('input').value
+	const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=kT7dlCmWgHXvQQNHbSy5dk2G96QAD9pQXb8Kjq8w')
+	const data = await res.json() // parse response as JSON
+		  console.log(data)
+		  document.querySelector('h2.nasapicoftheday').innerText = data.title
+		  document.querySelector('p.nasapicoftheday').innerText = data.explanation
+		  document.querySelector('img.nasapicoftheday').src = data.hdurl
+		  document.querySelector('span.nasapicoftheday').innerText = data.date
+  
+		  
+		}
+
+ 	 document.querySelector('#memeGen').addEventListener('click', getMemeGener)
+ 	 function getMemeGener(){
+   	 const choice = Number(document.querySelector('input').value)
+   	 const url = 'https://api.imgflip.com/get_memes'
+
+
+   fetch(url)
+       .then(res => res.json()) // parse response as JSON
+       .then(data => {
+         console.log(data)
+         // console.log(data.[{0}].word.meanings)
+         document.querySelector('h5').innerText = data.data.memes[choice].name
+         document.querySelector('#memeHere').src = data.data.memes[choice].url
+       })
+       .catch(err => {
+           console.log(`error ${err}`)
+       });
+ }
+
+// document.querySelector('#drinkGen').addEventListener('click', getADrink)
+
+// function getADrink(){
+//   const choice = document.querySelector('#aDrink').value
+//   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choice}`
+
+//   fetch(url)
+//       .then(res => res.json()) // parse response as JSON
+//       .then(data => {
+//         console.log(data.drinks[0])
+//         document.querySelector('#nameTheDrink').innerText = data.drinks[0].strDrink
+//         document.querySelector('#drinkImgHere').src = data.drinks[0].strDrinkThumb
+//         document.querySelector('#expTheDrink').innerText = data.drinks[0].strInstructions
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });
+// }
+
+
+//  document.querySelector('#spillTheBeans').addEventListener('click', getTopReddits)
+
+//  function getTopReddits(){
+//    const url2 = 'https://tradestie.com/api/v1/apps/reddit'
+
+//    fetch(url2)
+//        .then(res => res.json()) // parse response as JSON
+//        .then(data => {
+//  			console.log(data)
+//      //     document.querySelector('#nameTheDrink').innerText = data.drinks[0].strDrink
+//      //     document.querySelector('#drinkImgHere').src = data.drinks[0].strDrinkThumb
+//      //     document.querySelector('#expTheDrink').innerText = data.drinks[0].strInstructions
+//         })
+//        .catch(err => {
+//            console.log(`error ${err}`)
+//        });
+//  }
 
 
 
